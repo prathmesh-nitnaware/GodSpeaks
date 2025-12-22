e58236926d4791c48693a6cf6b69def37cd13322
Prathmesh Nitnaware <nitnaware.prathmesh@gmail.com>
2025-12-22 11:51:23 +0530

    Connect frontend and backend

--- godspeaks-frontend/src/api/orderApi.js
+++ godspeaks-frontend/src/api/orderApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // --- CONFIGURATION ---
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://godspeaks.onrender.com';
 const API_URL = `${API_BASE_URL}/api/orders`;
 
 /**
